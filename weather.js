0065cac083ebcac709de02f68429793e1d487466
AslanHuseynov <64832203+AslanHuseynov@users.noreply.github.com>
2020-05-11 23:43:08 +0400

    Update weather.js

--- weather.js
+++ weather.js
@@ -3,23 +3,25 @@ var main = document.querySelector('#name');
 var temp = document.querySelector('.temp');
 var desc = document.querySelector('.desc');
 var clouds = document.querySelector('.clouds');
-var button= document.querySelector('.submit');
+var button = document.querySelector('.submit');
+var card = document.querySelector('#city-card');
 
 
-button.addEventListener('click', function(name){
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=7a964a0acd57adfa7a660a56910c59cf')
-.then(response => response.json())
-.then(data => {
-  var tempValue = data['main']['temp'];
-  var nameValue = data['name'];
-  var descValue = data['weather'][0]['description'];
+button.addEventListener('click', function() {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&appid=50a7aa80fa492fa92e874d23ad061374&units=metric')
+        .then(response => response.json())
+        .then(data => {
+            var tempValue = data['main']['temp'];
+            var nameValue = data['name'];
+            var descValue = data['weather'][0]['description'];
 
-  main.innerHTML = nameValue;
-  desc.innerHTML = "Desc - "+descValue;
-  temp.innerHTML = "Temp - "+tempValue;
-  input.value ="";
+            card.classList.add('is-visible');
+            main.innerHTML = nameValue;
+            desc.innerHTML = "Desc - " + descValue;
+            temp.innerHTML = "Temp - " + tempValue;
+            input.value = "";
 
-})
+        })
 
-.catch(err => alert("Wrong city name!"));
-})
+    .catch(err => alert("Wrong city name!"));
+});
